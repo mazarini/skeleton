05edefe88c39f0baf73cc52fd234ec44ff95a9dd
Mazarini <johndoe@example.com>
2023-11-16 14:40:08 +0100

    Initial commit

---
+++ tests/DummyCommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class FileCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('app:dummy');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('[OK]', $output);
+    }
+}
