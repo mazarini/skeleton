d7079732ebde537288affb93ca32749917d6fe42
Mazarini <johndoe@example.com>
2025-01-24 00:12:05 +0100

    Move phpunit from 9.6 to 11.5

--- tests/Controller/HomepageControllerTest.php
+++ tests/Controller/HomepageControllerTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PHPUnit\Framework\Attributes\CoversNothing;
 
+#[CoversNothing()]
 final class HomepageControllerTest extends WebTestCase
 {
     public function testIndex(): void
