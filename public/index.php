ade8ac935abfdbc4c2c2bcab66793cee46103ba0
Mazarini <johndoe@example.com>
2025-01-24 12:46:25 +0100

    Setup phpstan Fix phpstan errors in PHP

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    if (!is_string($context['APP_ENV'])) {
+        throw new \RuntimeException('Environment variable "APP_ENV" must be a string.');
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
